082f9512a8e3c841e726d9c0a587feb7895b0677
marcelosobral <marceloflorenciosobral@gmail.com>
2014-12-11 01:30:22 -0600

    Removing cube code from the main file and the cleaning the orbit.js file (refactoring and adding comments)

--- step_0/orbit.js
+++ step_0/orbit.js
@@ -1,4 +1,18 @@
+// ============================================================================
+// ---   The Amazing Orbit   ---
+// ============================================================================
+// Here you will find all the variables, sources and functions 
+//  related to the planet orbits in the project.
+// Basically the orbits are points drawn in the center of the 
+//  planets as they move around the sun.
+// This the first .js file that looks like a class implementation from JS.
+// ============================================================================
+
 var Orbit = function() {
+
+	var theOrbitProgram = initShaders(gl, "orbit-vertex-shader", "orbit-fragment-shader");
+    gl.useProgram(theOrbitProgram);
+
 	this.VBOPoints = gl.createBuffer();
 	this.Index = 0;
 	this.orbitLength = 10000;
@@ -7,6 +21,7 @@ var Orbit = function() {
 	this.orbitSize = 1.0;
 	this.orbitColor = [ 128, 128, 0, 0.99 ];
 
+	// this function should be called to every planet/satellite moving on the system
 	this.addOrbitPos = function( pos ) {
 		if(this.Points.length >= this.orbitLength) {
             theOrbits.Points.splice(0, theOrbits.Points.length - theOrbits.orbitLength);
@@ -14,17 +29,17 @@ var Orbit = function() {
 		this.Points.push(pos)
 	}
 
-	var webglColor;
+	var webglColor;		// we only need this variable because dat.GUI library has a bug
 	this.drawOrbits = function(p, mv) {
-	    gl.useProgram(theCubeProgram);
+	    gl.useProgram(theOrbitProgram);
 		
-		gl.uniformMatrix4fv( gl.getUniformLocation(theCubeProgram, "projectionMatrix"),
+		gl.uniformMatrix4fv( gl.getUniformLocation(theOrbitProgram, "projectionMatrix"),
 			false, flatten(p));
 		   
-		gl.uniformMatrix4fv( gl.getUniformLocation(theCubeProgram, "modelViewMatrix"), 
+		gl.uniformMatrix4fv( gl.getUniformLocation(theOrbitProgram, "modelViewMatrix"), 
 			false, flatten(mv));   
 		
-		gl.uniform1f( gl.getUniformLocation(theCubeProgram, "orbitSize"), this.orbitSize);
+		gl.uniform1f( gl.getUniformLocation(theOrbitProgram, "orbitSize"), this.orbitSize);
 		
 		// we need this to correct the bug on dat.GUI
 		if(typeof this.orbitColor != "string") {
@@ -33,13 +48,13 @@ var Orbit = function() {
 			var thenums = this.orbitColor.match(/\d+/g);
 			webglColor = vec4(thenums[0]/255, thenums[1]/255, thenums[2]/255, thenums[4]/100);
 		}
-		gl.uniform4fv( gl.getUniformLocation(theCubeProgram, "orbitColor"), webglColor);
+		gl.uniform4fv( gl.getUniformLocation(theOrbitProgram, "orbitColor"), webglColor);
 
 	    gl.bindBuffer(gl.ARRAY_BUFFER, this.VBOPoints);
 	    gl.bufferData(gl.ARRAY_BUFFER, flatten(this.Points), gl.STATIC_DRAW); 
 		
 	    // Associate out shader variables with our data buffer
-	    var vPosition = gl.getAttribLocation(theCubeProgram, "vPosition");
+	    var vPosition = gl.getAttribLocation(theOrbitProgram, "vPosition");
 	    gl.bindBuffer(gl.ARRAY_BUFFER, this.VBOPoints);      
 	    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
 	    gl.enableVertexAttribArray(vPosition);
--- step_0/solar_system.js
+++ step_0/solar_system.js
@@ -209,78 +209,6 @@ function stopScale(x, y)
 {
     theScalingMove = false;
 }
-
-// ============================================================================
-// Cube related data structures and functions
-// ============================================================================
-var theCubeVBOPoints;
-var theCubeProgram;
-var theWireCubePoints = [];
-
-var theCubeVertices = [
-        vec4( -1.0, -1.0,  1.0, 1.0 ),
-        vec4( -1.0,  1.0,  1.0, 1.0 ),
-        vec4(  1.0,  1.0,  1.0, 1.0 ),
-        vec4(  1.0, -1.0,  1.0, 1.0 ),
-        vec4( -1.0, -1.0, -1.0, 1.0 ),
-        vec4( -1.0,  1.0, -1.0, 1.0 ),
-        vec4(  1.0,  1.0, -1.0, 1.0 ),
-        vec4(  1.0, -1.0, -1.0, 1.0 )
-    ];
-
-function wireQuad(a, b, c, d) 
-{
-     theWireCubePoints.push(theCubeVertices[a]); 
-     theWireCubePoints.push(theCubeVertices[b]); 
-     theWireCubePoints.push(theCubeVertices[c]); 
-     theWireCubePoints.push(theCubeVertices[d]);
-}
-
-function wireCube()
-{
-    wireQuad( 1, 0, 3, 2 );
-    wireQuad( 2, 3, 7, 6 );
-    wireQuad( 3, 0, 4, 7 );
-    wireQuad( 6, 5, 1, 2 );
-    wireQuad( 4, 5, 6, 7 );
-    wireQuad( 5, 4, 0, 1 );
-}
-
-function initCube()
-{
-    // initialize the cube
-    wireCube();
-	
-	// Load shaders and initialize attribute buffers
-    theCubeProgram = initShaders(gl, "orbit-vertex-shader", "orbit-fragment-shader");
-    gl.useProgram(theCubeProgram);
-    
-    // Create VBOs and load the data into the VBOs
-    theCubeVBOPoints = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, theCubeVBOPoints);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(theWireCubePoints), gl.STATIC_DRAW);   
-}
-
-function drawCube(p, mv) 
-{
-    gl.useProgram(theCubeProgram);
-	
-	gl.uniformMatrix4fv( gl.getUniformLocation(theCubeProgram, "projectionMatrix"),
-		false, flatten(p));
-	   
-	gl.uniformMatrix4fv( gl.getUniformLocation(theCubeProgram, "modelViewMatrix"), 
-		false, flatten(mv));   
-  
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(theCubeProgram, "vPosition");
-    gl.bindBuffer(gl.ARRAY_BUFFER, theCubeVBOPoints);      
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-    
-	for (var i = 0; i < 6; i++) {
-		gl.drawArrays(gl.LINE_LOOP, i * 4, 4);
-	}
-}
 	
 // ============================================================================
 // Sphere related data structures and functions
@@ -307,7 +235,7 @@ function sphereQuad(a, b, c, d)
 
 function initSphere()
 {
-    // initialize the cube
+    // initialize the square
 	sphereQuad(0, 1, 2, 3);
 	
 	// Load shaders and initialize attribute buffers
@@ -394,7 +322,6 @@ window.onload = function init()
 	initGUI();
     initSkyboxTextures();
     initSkybox();
-	initCube();
 	initSphere();
 	
     //render();
@@ -560,14 +487,7 @@ function render()
 	
     // draw the skybox first of all
     drawSkybox(p, mv);
-
-	// drawn for reference
-	//var cubeScale = scale(10, 10, 10);
-	//drawCube(p, mult(mv, cubeScale));
-	
-	if(theOrbits.visible){
-		theOrbits.drawOrbits(p, mv);
-	}
+	if(theOrbits.visible) theOrbits.drawOrbits(p, mv);
 	drawPlanets(p, mv);
 	
     requestAnimFrame( render );
